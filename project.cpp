8386a6168606d97f50e6f6d4943998565ff8cd0c
AhmedFarrukh <101267023+AhmedFarrukh@users.noreply.github.com>
2023-02-18 10:58:58 +0000

    initial structures  almost done

--- project.cpp
+++ project.cpp
@@ -1,28 +1,20 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include "course.h"
 using namespace std;
 
-class Course{
-    private:
-        string courseName;
-        int numOfStudents;
-        string[] studentList;
-        string facultyName;
-    public:
-        Course(string facultyName, courseName, numOfStudents,string[] studentList){
-            this->facultyName=facultyName;
-            this->courseName=courseName;
-            this->numOfStudents=numOfStudents;
-            for(int i=0;i<numOfStudents;i++){
-                this->studentList[i]=studentList[i];
-            }
-        }
-};
-
 int main() {
-    string studentList[]={"Fred Weasley", "George Wesley", "Oliver Wood", "Luna Lovegood", "Ron Weasley",
+
+    string studentNames[]={"Fred Weasley", "George Wesley", "Oliver Wood", "Luna Lovegood", "Ron Weasley",
     "Draco Malfoy", "Harry Potter", "Neville Longbottom", "Ginny Weasley", "Ernie MacMillan",
     "Percy Weasley", "Hermione Granger"};//takes the name of students
-    int numOfStudents=sizeof(studentList)/sizeof(studentList[0]);//counts number of students
+    int numOfStudents=sizeof(studentNames)/sizeof(studentNames[0]);//counts number of students
+    vector<Students> studentList;//holds the student objects
+    for(int i=0;i<numOfStudents;i++){
+        studentList.push_back(Students(studentNames[i]));
+    }//creates student objects
+    Course myCourse("Snape","Potions",numOfStudents,studentList);//creates course with these students
+    myCourse.printClassList();
     return 0;
 }
