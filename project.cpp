cb1eb6194456d20e60f1cc47a689e2c399d113bf
AhmedFarrukh <101267023+AhmedFarrukh@users.noreply.github.com>
2023-02-28 17:27:40 +0000

    testing

--- project.cpp
+++ project.cpp
@@ -1,20 +1,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "course.h"
+#include "cms.h"
 using namespace std;
 
 int main() {
-
-    string studentNames[]={"Fred Weasley", "George Wesley", "Oliver Wood", "Luna Lovegood", "Ron Weasley",
-    "Draco Malfoy", "Harry Potter", "Neville Longbottom", "Ginny Weasley", "Ernie MacMillan",
-    "Percy Weasley", "Hermione Granger"};//takes the name of students
-    int numOfStudents=sizeof(studentNames)/sizeof(studentNames[0]);//counts number of students
-    vector<Students> studentList;//holds the student objects
-    for(int i=0;i<numOfStudents;i++){
-        studentList.push_back(Students(studentNames[i]));
-    }//creates student objects
-    Course myCourse("Snape","Potions",numOfStudents,studentList);//creates course with these students
-    myCourse.printClassList();
+    Course A("OOP","Object oriented programming");
     return 0;
 }
