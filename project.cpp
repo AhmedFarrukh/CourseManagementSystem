82fc9b9c6534b403ef348838f7afa5bd49c42756
AhmedFarrukh <101267023+AhmedFarrukh@users.noreply.github.com>
2023-03-01 13:57:35 +0000

    main functions working, private access error

--- project.cpp
+++ project.cpp
@@ -3,35 +3,36 @@
 #include <vector>
 #include "cms.h"
 using namespace std;
-
+inline void print(int i, string s){cout<<i<<"  "<<s<<endl;}
 int main() {
+    //Creating Courses
     Course A("Portions","Learn the art of brewing powerful potions!");
     Course B("Ancient Magic","Learn more about the lives of ancient witches and wizards!");
     Course C("Defence Against Dark Arts","Learn to protect yourself against evil!");
     Course D("Herbology","Learn about magical plants and their uses!");
+    //Creating vectors of courses for each faculty object
+    vector <Course*> teach1;
+    teach1.push_back(&A);
 
-    vector <Course> teach1;
-    teach1.push_back(A);
+    vector <Course*> teach2;
+    teach2.push_back(&B);
 
-    vector <Course> teach2;
-    teach2.push_back(B);
+    vector <Course*> teach3;
+    teach3.push_back(&C);
+    teach3.push_back(&D);
 
-    vector <Course> teach3;
-    teach3.push_back(C);
-    teach3.push_back(D);
-
-
+    //creating faculty objects
     vector <Faculty> faculty;
     faculty.push_back(Faculty ("Horace Slughorn", teach1));
     faculty.push_back(Faculty ("Eleazar Fig", teach2));
     faculty.push_back(Faculty ("Severus Snape",teach3));
-
-    vector <Course> courses;
-    courses.push_back(A);
-    courses.push_back(B);
-    courses.push_back(C);
-    courses.push_back(D);
-
+    //creating a vector of all courses offered
+    vector <Course*> courses;
+    courses.push_back(&A);
+    courses.push_back(&B);
+    courses.push_back(&C);
+    courses.push_back(&D);
+    //creating students and assigning them classes
     vector <Student> students;
     students.push_back(Student("Harry Potter", courses));
     students.push_back(Student("Ron Weasley", courses));
@@ -40,11 +41,62 @@ int main() {
     students.push_back(Student("Cedric Diggory", courses));
     students.push_back(Student("Luna Lovegood", courses));
     students.push_back(Student("Dean Thomas", courses));
-    // courses[0].printCourseDetails();
-    // courses[1].printCourseDetails();
-    // courses[2].printCourseDetails();
-    // courses[3].printCourseDetails();
 
+    char view_selection='S';
     cout<<"Welcome to Hogwarts School of Witchcraft and Wizardry!"<<endl;
+    do{
+        cout<<"You may view the Course Management System as a Student or as a Faculty Member"<<endl;
+        cout<<"Press 'S' for Student View"<<endl;
+        cout<<"Press 'F' for Faculty View"<<endl;
+        cout<<"Press 'E' to exit the system"<<endl;
+        cin>>view_selection;
+        if(view_selection=='s'||view_selection=='S'){
+            //Student View
+            //Print List of Students.
+            int student_select;
+            cout<<"Please enter the index for your name!"<<endl;
+            for(int i=0;i<students.size();i++){
+                cout<<i<<"  "<<students[i].getName()<<endl;
+            }
+            cin>>student_select;
+            //print courses for the student selected
+            int course_select;
+            cout<<"Please enter the index for the course whose details are to be accessed"<<endl;
+            students[student_select].printCourses();
+            cin>>course_select;
+            //print the different functions available for students
+            int option_select;
+            cout<<"Please note the options below and enter the appropriate number to proceed"<<endl;
+            print(0,"View Course Description");
+            print(1,"View Course Details");
+            print(2,"View the Syllabus");
+            print(3,"View Course Material");
+            print(4,"View Grade");
+            cin>>option_select;
+            if(option_select==0){
+                students[student_select].courses[course_select]->printCourseDescription();
+            }
+            else if(option_select==1){
+                students[student_select].courses[course_select]->printCourseDetails();
+            }
+            else if(option_select==2){
+                students[student_select].courses[course_select]->printSyllabus();
+            }
+            else if(option_select==3){
+                students[student_select].courses[course_select]->printCourseMaterials();
+            }
+            else if(option_select==4){
+                cout<<students[student_select].grades[course_select]<<endl;
+            }
+            else {cout<<"Incorrect option was selected"<<endl;}
+        }
+        else if(view_selection=='f'||view_selection=='F'){
+
+        }
+        else if(view_selection!='e'||view_selection!='E'){break;}
+        else{cout<<"You have selected an incorrect option"<<endl;}
+
+    }while(view_selection!='e'||view_selection!='E');
+
     return 0;
 }
