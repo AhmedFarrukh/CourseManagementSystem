ff50bc0cdf66f324ceab3b22a31afc591a0584ce
AhmedFarrukh <101267023+AhmedFarrukh@users.noreply.github.com>
2023-03-03 12:50:13 +0000

    almost done

--- Report.md
+++ Report.md
@@ -4,7 +4,7 @@
 
 The aim of my code is to create a simple version for a course management system. 
 
-A course management system is a software tool used to host course information such as syllabi and provide a medium of interaction between teachers and learners<sup>[1]</sup>. In addition, an effective cours management system is supposed to host all the neccessary details of a course, allowing a single platform to be acccessed for all resources regarding a course<sup>[2]</sup>. While a course management system may provide remote learners the only platform for instruction, even in-person teaching enviornmnets greatly benefit from the presence of an online course management system<sup>[3]</sup>.
+A course management system is a software tool used to host course information such as syllabi and provide a medium of interaction between teachers and learners<sup>[1]</sup>. In addition, an effective course management system is supposed to host all the neccessary details of a course, allowing a single platform to be acccessed for all resources regarding a course; it is different from a learning management system, which is generally made specifically for remote learning<sup>[2]</sup>. Course management systems are helpful for both remote and in-person teaching enviornments as both greatly benefit from the presence of online access to course resources<sup>[3]</sup>.
 
 The program should store data about courses, allow students to view information about the courses they are enrolled in and allow faculty to view and edit information about the courses that they are teaching. In addition, the program should also mantain data about which courses are offered, who is teaching them and who is enrolled in these courses. A course management system, the likes of which are used in universities, mantains large sets of data that needs to be stored orderly<sup>[4]</sup>. This challenge needs to be tackled using effective ways of storing and sharing information. 
 
@@ -143,7 +143,7 @@ Here are some of the key functions inside declared classes.
 - printSyllabus in Course
 ```c++
 void printSyllabus(){//prints the contents of the syllabus file. if wrong name is input, or no name input yet, throws an exception
-     try{
+            try{
             ifstream inFile;
             inFile.open(syllabusFile,ios::in);
             if(inFile.fail()){
@@ -164,21 +164,22 @@ The printSyllabus function, similar to the printCourseMaterial function prints t
 
 - print classGrades in Course
 ```c++
-void printClassGrades(){
-     double sum=0;
-     int j;
-     cout<<"The grades are as follows: "<<endl;
-     for(int i=0;i<students.size();i++){//prints the grade for each student
-           for(j=0;j<students[i]->getNumOfCoursesEnrolled();j++){//find the relevant index of course in the courses vector held by the student
-           if(students[i]->getCourse(j).getCourseName()==courseName){
-           break;
-           }
-           }
-      cout<<setw(40)<<left<<students[i]->getName()<<setw(5)<<left<<students[i]->getGrade(j)<<endl;//make pretty
-      sum+=students[i]->getGrade(j);
-      }
-      cout<<setw(40)<<left<<"The average grade is "<<sum/(double)students.size()<<endl;
-      }
+        void printClassGrades(){
+            double sum=0;
+            int j;
+            cout<<"The grades are as follows: "<<endl;
+            for(int i=0;i<students.size();i++){//prints the grade for each student
+                    for(j=0;j<students[i]->getNumOfCoursesEnrolled();j++){//find the relevant index of course in the courses vector held by the student
+                        if(students[i]->getCourse(j).getCourseName()==courseName){
+                        break;
+                        }
+                    }
+                cout<<setw(40)<<left<<students[i]->getName()<<setw(5)<<left<<students[i]->getGrade(j)<<endl;//make pretty
+                sum+=students[i]->getGrade(j);
+            }
+            cout<<setw(40)<<left<<"The average grade is "<<sum/(double)students.size()<<endl;
+        }
+        string getCourseName(){return courseName;}
 ```
 This function prints the grades for each student in a class. Using a for loop, it cycles through every student in the students vector held by the course. Then it compares each course taken by the student to the course in question, to find the index of this course in the students list of courses. The function then gets the grade at the index found through the last step. Additionally, it also calculates the average class grade.
 
@@ -230,50 +231,8 @@ Example: Line 15 of the header file:
 Student(string name, vector<Course*>& courses);
 ```
 
-- Prefix Imcrement
-Variables have been prefix incremented (++i, instead of i++) as postfix increment leads to the creation of a temporary variable that slows down the program. This may be a minor optimization but since incrementing taking place so frequently through the code, the effects add up.
-Example: Line 53 of the main file
-```c++
-for(int i=0;i<students.size();++i)
-```
 
-#### OOP Concepts
-- Inheritance: Both Faculty and Student classes are inherited from the Member class. The Member class has two important variables (name and course vector) that are common to both classes. It also makes intuitive sense since both faculty and students are member of the university system.
-```c++
-class Student: private Member{
-```
-- Friendhsip: The Faculty class is a friend of the Course class so that it may edit the variables of the Course class. 
-```c++
-class Course{
-friend class Faculty
-```
-- Static Variable: The number of courses has been declared as a static variable, since it should be outside a specific object and needs to count each instance when an object is created.
-```c++
- static int numOfCourses;
-```
-- 'this' Keyword: The this keyword has extensively been used in constructors to refer to the object whose constructor has been called.
-```c++
-this->courseName=courseName;
-```
-- 'throw-exception': Exceptions have been used when opening files, incase the file name is incorrect or yet to be updated.
-```c++
-try{
-    ifstream inFile;
-    inFile.open(syllabusFile,ios::in);
-    if(inFile.fail()){
-       throw(syllabusFile);
-       }
-       char character;
-       while(!inFile.eof()){
-        character=inFile.get();
-        if(character==inFile.eof()){break;}
-        cout<<character;
-        }
-       cout<<endl;
-       inFile.close();
-       }catch(string syllabusFile){cout<<"There was an error opening the syllabus file.\n Note that the syllabus is only available once the instructor has uploaded it."<<endl;}
-        }
-```
+
 #### References
 <sup>[1]</sup> https://cft.vanderbilt.edu/guides-sub-pages/course-management-systems/#:~:text=A%20course%20management%20system%20
 
---
+++ code/project.cpp
@@ -0,0 +1,208 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include "cms.h"
+using namespace std;
+//inline functions for optimized printing
+inline void print(int i, string s){cout<<i<<"  "<<s<<endl;}
+int main() {
+    //Up until the first print statement, the code creates student, faculty and course objects. 
+    //While this is, ofcourse, neccessary for the project, it is not the main focus as the project is a learning management system 
+    //that is assumed to have already recieved this data. 
+    //Creating Courses
+    Course A("Portions","Learn the art of brewing powerful potions!");
+    Course B("Ancient Magic","Learn more about the lives of ancient witches and wizards!");
+    Course C("Defence Against Dark Arts","Learn to protect yourself against evil!");
+    Course D("Herbology","Learn about magical plants and their uses!");
+    //Creating vectors of courses for each faculty object. 
+    //A faculty member can teach more than one course.
+    //All course vectors throughout the code contain pointers to vectors to 'pass by reference' instead of copies.
+    vector <Course*> teach1;
+    teach1.push_back(&A);
+
+    vector <Course*> teach2;
+    teach2.push_back(&B);
+
+    vector <Course*> teach3;
+    teach3.push_back(&C);
+    teach3.push_back(&D);
+
+    //creating faculty objects and assigning their courses
+    vector <Faculty> faculty;
+    faculty.push_back(Faculty ("Horace Slughorn", teach1));
+    faculty.push_back(Faculty ("Eleazar Fig", teach2));
+    faculty.push_back(Faculty ("Severus Snape",teach3));
+    //creating a vector of pointers all courses offered
+    vector <Course*> courses;
+    courses.push_back(&A);
+    courses.push_back(&B);
+    courses.push_back(&C);
+    courses.push_back(&D);
+    //creating students and assigning them classes
+    vector <Student> students;
+    students.push_back(Student("Harry Potter", courses));
+    students.push_back(Student("Ron Weasley", courses));
+    students.push_back(Student("Hermione Granger", courses));
+    students.push_back(Student("Neville Longbottom", courses));
+    students.push_back(Student("Cedric Diggory", courses));
+    students.push_back(Student("Luna Lovegood", courses));
+    students.push_back(Student("Dean Thomas", courses));
+    //creating a vector of student objects - to be passed to courses
+    vector<Student*> studentptrs;
+    for(int i=0;i<students.size();++i){
+        studentptrs.push_back(&students[i]);
+    }
+    //adding students to courses
+    courses[0]->addStudents(studentptrs);
+    courses[1]->addStudents(studentptrs);
+    courses[2]->addStudents(studentptrs);
+    courses[3]->addStudents(studentptrs);
+    //the course management system interface starts here.
+    //the use has the option to view as either a faculty or a student member
+    //Faculty can edit and view information
+    //Students can only view information 
+    char view_selection='S';
+    cout<<"Welcome to Hogwarts School of Witchcraft and Wizardry!"<<endl;
+    cout<<endl;
+    do{
+        cout<<"You may view the Course Management System as a Student or as a Faculty Member"<<endl;
+        cout<<"Press 'S' for Student View"<<endl;
+        cout<<"Press 'F' for Faculty View"<<endl;
+        cout<<"Press 'E' to exit the system"<<endl;
+        cin>>view_selection;
+        cout<<endl;
+        if(view_selection=='s'||view_selection=='S'){
+            //Student View
+            //Print List of Students.
+            int student_select;
+            cout<<"Please enter the index for your name!"<<endl;
+            for(int i=0;i<students.size();++i){
+                print(i,students[i].getName());
+            }
+            cin>>student_select;
+            cout<<endl;
+            //print courses for the student selected
+            int course_select;
+            do{
+            cout<<"Please enter the index for the course whose details are to be accessed"<<endl;
+            students[student_select].printCourses();
+            print(-1,"Exit Student View");//the user should enter -1 to exit the student view
+            cin>>course_select;
+            cout<<endl;
+            if(course_select==-1){break;}
+            //print the different functions available for students
+            int option_select;
+            cout<<"Please note the options below and enter the appropriate number to proceed"<<endl;
+            print(0,"View Course Description");
+            print(1,"View Course Details");
+            print(2,"View the Syllabus");
+            print(3,"View Course Material");
+            print(4,"View Grade");
+            cin>>option_select;
+            cout<<endl;
+            if(option_select==0){//print course description
+                students[student_select].getCourse(course_select).printCourseDescription();
+                cout<<endl;
+            }
+            else if(option_select==1){//print course details - name, faculty, description
+                students[student_select].getCourse(course_select).printCourseDetails();
+                cout<<endl;
+            }
+            else if(option_select==2){//print the syllabus. only if uploaded earlier in faculty view.
+                students[student_select].getCourse(course_select).printSyllabus();
+                cout<<endl;
+            }
+            else if(option_select==3){//print course material. only if uploaded earlier in faculty view.
+                students[student_select].getCourse(course_select).printCourseMaterials();
+                cout<<endl;
+            }
+            else if(option_select==4){//show the grade for the class
+                cout<<students[student_select].getGrade(course_select)<<endl;
+                cout<<endl;
+            }
+            else {cout<<"Incorrect option was selected"<<endl;}
+            }while(course_select!=-1);
+        }
+        else if(view_selection=='f'||view_selection=='F'){
+            //faculty view
+            int faculty_select;
+            //selecting the appropriate course - faculty can teach more than one
+            cout<<"Please select the index for your name"<<endl;
+            for(int i=0;i<faculty.size();++i){
+                print(i,faculty[i].getName());
+            }
+            cin>>faculty_select;
+            cout<<endl;
+            //selecting the correct function for the course
+            int course_select;
+                cout<<"Please select the index for the course whose information is to be viewed/edited"<<endl;
+                faculty[faculty_select].printCourses();
+                cin>>course_select;
+                cout<<endl;
+                int option_select;
+            do{
+                cout<<"Please select the appropriate option to proceed"<<endl;
+                print(0,"View Course Description");
+                print(1,"View Course Details");
+                print(2,"View the Syllabus");
+                print(3,"View the Course Materials");
+                print(4,"View Class Grades");
+                print(5,"Edit Course Description");
+                print(6,"Upload Syllabus");
+                print(7,"Upload Course Materials");
+                print(8,"Set Class Grades");
+                print(9,"Print Class List");
+                print(-1,"Exit Faculty View");
+                if(option_select==-1){break;}
+                cin>>option_select;
+                cout<<endl;
+                if(option_select==0){//prints course description
+                    faculty[faculty_select].getCourse(course_select).printCourseDescription();
+                    cout<<endl;
+                }
+                else if(option_select==1){//prints course details
+                    faculty[faculty_select].getCourse(course_select).printCourseDetails();
+                    cout<<endl;
+                }
+                else if(option_select==2){//prints syllabus - if uplaoded
+                    faculty[faculty_select].getCourse(course_select).printSyllabus();
+                    cout<<endl;
+                }
+                else if(option_select==3){//prints course material - if uploaded
+                    faculty[faculty_select].getCourse(course_select).printCourseMaterials();
+                    cout<<endl;
+                }
+                else if(option_select==4){//prints the grades for everyone in class
+                    faculty[faculty_select].getCourse(course_select).printClassGrades();
+                    cout<<endl;
+                }
+                else if(option_select==5){//enables change in course description
+                    faculty[faculty_select].setCourseDescription(course_select);
+                    cout<<endl;
+                }
+                else if(option_select==6){//enables uploading a syllabus file
+                    faculty[faculty_select].setSyllabus(course_select);
+                    cout<<endl;
+                }
+                else if(option_select==7){//enables uploading a course materials file
+                    faculty[faculty_select].setCourseMaterialFile(course_select);
+                    cout<<endl;
+                }
+                else if(option_select==8){//enables setting grades for everyone in class
+                    faculty[faculty_select].setGrades(course_select);
+                    cout<<endl;
+                }
+                else if(option_select==9){//prints the list for everyone in class
+                    faculty[faculty_select].getCourse(course_select).printClassList();
+                    cout<<endl;
+                }
+                else {cout<<"You have made the wrong selection"<<endl;}
+            }while(option_select!=-1);
+        }
+        else if(view_selection!='e'||view_selection!='E'){break;}//if e/E is selected when asked about student/faculty view, the program terminates.
+        else{cout<<"You have selected an incorrect option"<<endl;}
+
+    }while(view_selection!='e'||view_selection!='E');
+    return 0;
+}
